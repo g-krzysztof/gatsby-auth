422684979e0998f3dec662229fb8bb42dd281a59
Krzysztof Gryc <hello@krzysztofg.pl>
2020-11-20 14:23:19 +0100

    fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,20 +20,23 @@ exports.onCreatePage = async ({ page, actions }) => {
 const path = require('path')
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html" || stage === "develop-html") {
-    const regex = [
-      /node_modules\/leaflet/,
-      /node_modules\\leaflet/
-    ]
+  // if (stage === "build-html" || stage === "develop-html") {
+  //   const regex = [
+  //     /node_modules\/leaflet/,
+  //     /node_modules\\leaflet/
+  //   ]
     actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: regex,
-            use: loaders.null()
-          }
-        ]
-      }
+      node: {
+        fs: 'empty',
+      },
+      // module: {
+      //   rules: [
+      //     {
+      //       test: regex,
+      //       use: loaders.null()
+      //     }
+      //   ]
+      // }
     })
-  }
+  // }
 }
